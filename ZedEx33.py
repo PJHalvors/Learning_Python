e7bebf331d403fa3e5b373902aa973e7547833a9
PJ Halvors <priyajhalvorsen@gmail.com>
2018-03-02 20:05:57 -0500

    Write ex33.py

---
+++ ZedEx33.py
@@ -0,0 +1,19 @@
+#!/bin/env/python
+
+#Exercise 33
+
+i = 0
+numbers = []
+
+while i < 6:
+  print "At the top of i is %d" %i
+  number.append(i)
+  
+  i = i + 1
+  print "Numbers now:", numbers
+  print "At the bottom of i is %d" % i
+
+print "The numbers:"
+
+for num in numers:
+  print num
