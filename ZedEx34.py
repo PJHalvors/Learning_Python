6b18981f13ee96bc0a2f745e77b8a2a0b9a3afa4
PJ Halvors <priyajhalvorsen@gmail.com>
2018-03-02 20:01:43 -0500

    Add files via upload

---
+++ ZedEx34.py
@@ -0,0 +1,36 @@
+#!/bin/env/python
+
+#Set List "animals" from exercise 34
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print""
+
+#Quiz questions to identify animals
+print """1. The animal at 1.
+2. The third animal.
+3. The first animal.
+4. The animal at 3.
+5. The fifth animal.
+6. The animal at 2.
+7. The sixth animal.
+8. The animal at 4."""
+
+print""
+
+print "1.", animals[1]
+print "2.", animals[2]
+print "3.", animals[0]
+print "4.", animals[3]
+print "5.", animals[4]
+print "6.", animals[2]
+print "7.", animals[5]
+print "8.", animals[4]
+
+print""
+
+print "The animal at 0 is the first animal."
+print "The animal at 1 is the second animal."
+print "The animal at 2 is the third animal."
+print "The animal at 3 is the fourth animal."
+print "The animal at 4 is the fifth animal."
+print "The animal at 5 is the sixth animal."
